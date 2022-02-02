c8e8f286d265e70713a5c4c10f54a3f0a5cf2136
Alexandr Polikarpov <nub.and.co@gmail.com>
2022-02-03 02:57:42 +0300

    Первый комит

---
+++ src/HomeWork/Book.java
@@ -0,0 +1,29 @@
+package HomeWork;
+
+public class Book {
+    String bookName;
+    Author authorName;
+    int publishingYear;
+
+    public Book(String bookName, Author authorName, int publishingYear) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Author getAuthorName() {
+        return authorName;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+}
